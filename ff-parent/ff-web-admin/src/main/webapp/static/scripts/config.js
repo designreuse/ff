7fc5174f46524d6717744e22e70f9282a52e504f
dblazevic <darko.blazevic@nth.ch>
2017-04-10 16:28:50 +0200

    Version changed to 1.0.11

--- ff-parent/ff-web-admin/src/main/webapp/static/scripts/config.js
+++ ff-parent/ff-web-admin/src/main/webapp/static/scripts/config.js
@@ -808,7 +808,7 @@ angular.module('FundFinder')
 	$rootScope.$state = $state;
 	$rootScope.$stateParams = $stateParams;
 	
-	$rootScope.version = "v1.0.10";
+	$rootScope.version = "v1.0.11";
 	$rootScope.contextPath = constants.contextPath;
 	$rootScope.helpEnabled = false; // indicates if help feature is enabled or not
 	
