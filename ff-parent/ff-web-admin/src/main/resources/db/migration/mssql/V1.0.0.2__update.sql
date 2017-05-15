ff495b22e0403f7d70bdd2047d0a5c3025d22990
Darko Blazevic <kincade1975@gmail.com>
2017-05-15 10:50:15 +0200

    Updating sql script v1.0.0.2

--- ff-parent/ff-web-admin/src/main/resources/db/migration/mssql/V1.0.0.2__update.sql
+++ ff-parent/ff-web-admin/src/main/resources/db/migration/mssql/V1.0.0.2__update.sql
@@ -1,6 +1,8 @@
 ALTER TABLE [dbo].[company] ADD [sync_data] [bit] NOT NULL DEFAULT 0;
 ALTER TABLE [dbo].[company] ADD [hide_sync_data_warning] [bit] NOT NULL DEFAULT 0;
 
+ALTER TABLE [dbo].[company_item] ADD [value_ext] [ntext] NULL;
+
 ALTER TABLE [dbo].[item] ADD [emphasize] [bit] NOT NULL DEFAULT 0;
 
 ALTER TABLE [dbo].[item_option] ADD [url] [ntext] NULL;
--- ff-parent/ff-web-admin/src/main/resources/db/migration/mysql/V1.0.0.2__update.sql
+++ ff-parent/ff-web-admin/src/main/resources/db/migration/mysql/V1.0.0.2__update.sql
@@ -1,6 +1,8 @@
 ALTER TABLE company ADD sync_data tinyint(1) DEFAULT 0;
 ALTER TABLE company ADD hide_sync_data_warning tinyint(1) DEFAULT 0;
 
+ALTER TABLE company_item ADD value_ext longtext;
+
 ALTER TABLE item ADD emphasize tinyint(1) DEFAULT 0;
 
 ALTER TABLE item_option ADD url longtext;
