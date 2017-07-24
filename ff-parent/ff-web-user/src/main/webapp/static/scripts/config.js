87c2b8df105204efa11445d740f0608d20cfa824
Darko Blazevic <kincade1975@gmail.com>
2017-07-24 10:12:36 +0200

    v1.0.17

--- ff-parent/ff-web-user/src/main/webapp/static/scripts/config.js
+++ ff-parent/ff-web-user/src/main/webapp/static/scripts/config.js
@@ -247,7 +247,7 @@ angular.module('FundFinder')
 	$rootScope.$state = $state;
 	$rootScope.$stateParams = $stateParams;
 	
-	$rootScope.version = "v1.0.16";
+	$rootScope.version = "v1.0.17";
 	$rootScope.helpEnabled = false; // indicates if help feature is enabled or not
 	
 	$locale.NUMBER_FORMATS.DECIMAL_SEP= ',';
