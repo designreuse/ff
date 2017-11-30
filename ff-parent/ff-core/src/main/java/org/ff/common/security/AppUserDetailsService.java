62a43805e769526f8fcf010e023ca4156e46a5c9
Darko Blazevic <kincade1975@gmail.com>
2017-11-30 15:51:43 +0100

    additional log point introduced

--- ff-parent/ff-core/src/main/java/org/ff/common/security/AppUserDetailsService.java
+++ ff-parent/ff-core/src/main/java/org/ff/common/security/AppUserDetailsService.java
@@ -47,7 +47,7 @@ public class AppUserDetailsService implements UserDetailsService {
 			throw new UsernameNotFoundException("User not found!");
 		}
 
-		log.debug("User with username [{}] successfully authenticated", username);
+		log.debug("User with username [{}] found", username);
 
 		return new AppUserDetails(appUser);
 	}
